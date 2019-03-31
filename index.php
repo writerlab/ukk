3db27a4423405c9a4999c1c2feea828f96f6b837
writerlab <netspytux@gmail.com>
2019-04-01 00:42:02 +0700

    update...

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
+session_start();
+include('koneksi.php');
 include("konten/header.html");
 ?>
 <body>
   
   <div class="container">
     <?php
-    include("konten/judul.html"); ?>  
+    if (empty($_SESSION['id'])) {
+      include('konten/login.php');
+    } else {
+      include("konten/judul.html"); ?>  
 
-    <!-- menu & konten -->
-    <div class="row">
-      <?php
-      // menu
-      include("konten/menu.html");
+      <!-- menu & konten -->
+      <div class="row">
+        <?php
+        // menu
+        include("konten/menu.html");
 
-      // konten
-      include('atur-konten.php'); ?>
+        // konten
+        include('atur-konten.php');
+    }
+     ?>
     </div>
   </div>
 
