c91d47c51032ea507385d67e702dc9e1a0fb23b6
writerlab <netspytux@gmail.com>
2019-04-11 13:34:28 +0700

    update UI

--- konten/login.php
+++ konten/login.php
@@ -21,22 +21,31 @@ if (isset($_POST['masuk'])) {
 }
 ?>
 
-<div class="col-4 offset-md-4">
-  <div class="card">
-    <div class="card-header">
-      Login ke Aplikasi
-    </div>
-    <div class="card-body">
-      <?php print $pesan?>
-      <form action="" method="post">
-        <div class="form-group">
-          <input type="text" class="form-control" name="username" placeholder="username" required>
+<section class="login">
+  <div class="container">
+    <div class="row">
+      <div class="col-4 offset-md-4">
+        <h2 class="text-center pt-5 pb-5 text-white">
+          APLIKASI INVENTARIS SARPRAS SMK.
+        </h2>
+        <div class="card">
+          <div class="card-header">
+            Masuk ke Aplikasi.
+          </div>
+          <div class="card-body">
+            <?php print $pesan?>
+            <form action="" method="post">
+              <div class="form-group">
+                <input type="text" class="form-control" name="username" placeholder="username" autofocus required>
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" name="password" placeholder="password" required>
+              </div>
+              <button type="submit" class="btn btn-info" name="masuk">Masuk</button>
+            </form>
+          </div>
         </div>
-        <div class="form-group">
-          <input type="password" class="form-control" name="password" placeholder="password" required>
-        </div>
-        <button type="submit" class="btn btn-info" name="masuk">Masuk</button>
-      </form>
+      </div>
     </div>
   </div>
-</div>
+</section>
